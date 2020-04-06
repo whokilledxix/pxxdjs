f6f8a9f407cc6654f484a37feea68cc4b9e4088b
Glitch (hello-express) <none>
2020-04-06 15:30:07 +0000

    👩🎭 Checkpoint ./index.js:1706941/2382 ./events/ready.js:1706941/1302

--- index.js
+++ index.js
@@ -81,6 +81,29 @@ bot.on("message", async message => {
     message.reply('Nie znaleziono komendy! wpisz ``k!pomoc`` aby zobaczyć listę komend!')
 
 }
+  if (message.author.bot) return;
+  if (message.mentions.users.array().length > 0) {
+    if (message.member.hasPermission('ADMINISTRATOR')) return;
+    const exampleEmbed = new Discord.MessageEmbed()
+      .setColor("#0055ff")
+      .setTitle("Użytkownik usunął wiadomość zawierającą wzmiankę:")
+      .setDescription(message.content)
+      .setAuthor(message.author.tag, message.author.displayAvatarURL())
+      .setTimestamp();
+    message.channel.send(exampleEmbed);
+  }
+  let eembed = new Discord.MessageEmbed()
+    .setTitle("**Usunięta wiadomość**")
+    .setColor(colours.red)
+    .setThumbnail(message.author.displayAvatarURL())
+    .addField("Autor", `${message.author} | ${message.author.tag}`, true) //dodaje wzmiankę użytkownika w celu zidentyfikowania go po zmianie nicku.
+    .addField("Kanał", `${message.channel}`, true)
+    .addField("Treść wiadomości",`${message.content ? message.content : "Brak"}`)
+    .setTimestamp()
+    .setFooter(`ID: ${message.id}`);
+  let channel = message.guild.channels.cache.find(x => x.name === "logs");
+  channel.send(eembed);
+ 
 }
  ) 
  
